bcd7f12f0b00aff2370d6cfbfbe17fdaf3d9f5e4
neha rawat <neha.rawat@tigeranalytics.com>
2024-09-04 18:38:51 +0530

    Changes file to make the test run

---
+++ tests/test_module1.py
@@ -0,0 +1,38 @@
+# tests/test_module1.py
+import os
+
+import sys
+
+import unittest
+
+import pandas as pd  # type: ignore
+
+from src.nonstandardcode import income_cat_proportions
+
+sys.path.append(os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+
+class TestIncomeCatProportions(unittest.TestCase):
+
+    def setUp(self):
+        """Set up a simple DataFrame for testing."""
+        self.data = pd.DataFrame({
+            "income_cat":
+            ["high", "medium", "high", "low",
+                "medium", "medium", "low", "high"]
+        })
+
+    def test_income_cat_proportions(self):
+        """Test the income_cat_proportions function."""
+        result = income_cat_proportions(self.data)
+        expected = pd.Series({
+            "high": 0.375,
+            "medium": 0.375,
+            "low": 0.25
+        })
+        pd.testing.assert_series_equal(result, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
